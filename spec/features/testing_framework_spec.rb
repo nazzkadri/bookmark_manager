6673d5a09278880172e9c66d2c106c13e8117598
Naziya Kadri <nazzkadri@MacBook-Pro-67.local>
2018-09-25 17:32:22 +0100

    step 9 completed

--- spec/features/testing_framework_spec.rb
+++ spec/features/testing_framework_spec.rb
@@ -9,9 +9,13 @@ end
 feature 'Viewing bookmarks' do
   scenario 'A user can see bookmarks' do
     visit('/bookmarks')
-
-    expect(page).to have_content "http://www.makersacademy.com"
-    expect(page).to have_content "http://www.destroyallsoftware.com"
-    expect(page).to have_content "http://www.google.com"
+    connection = PG.connect(dbname: 'bookmark_manager_test')
+    connection.exec("INSERT INTO bookmarks (url) VALUES ('http://www.makersacademy.com');")
+    connection.exec("INSERT INTO bookmarks (url) VALUES('http://www.destroyallsoftware.com');")
+    connection.exec("INSERT INTO bookmarks (url) VALUES('http://www.google.com');")
+    bookmarks = Bookmark.all
+    expect(bookmarks).to have_content "http://www.makersacademy.com"
+    expect(bookmarks).to have_content "http://www.destroyallsoftware.com"
+    expect(bookmarks).to have_content "http://www.google.com"
   end
 end
