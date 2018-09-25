a35754d172a541cdd0671741307a68dfc7474ff2
Naziya Kadri <nazzkadri@MacBook-Pro-67.local>
2018-09-25 17:16:25 +0100

    spec helper changed to include env

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,8 +17,14 @@
 # at the top of spec/spec_helper.rb
 
 # Set the environment to "test"
+require_relative './setup_test_database'
 
 ENV['ENVIRONMENT'] = 'test'
+RSpec.configure do |config|
+  config.before(:each) do
+    setup_test_database
+  end
+end
 
 # Bring in the contents of the `app.rb` file
 require File.join(File.dirname(__FILE__), '..', 'app.rb')
